ffa1e7b0312dfad7b00058eab7923307c55da6ba
Bikash Pokhrel <87564637+thebikashpokhrel@users.noreply.github.com>
2024-10-19 18:21:05 +0545

    Updated constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "thebikash Blog";
 export const SITE_DESCRIPTION =
   "a random mix of programming, engineering, and science";
-export const ARTICLES_PER_PAGE = 1;
+export const ARTICLES_PER_PAGE = 6;
